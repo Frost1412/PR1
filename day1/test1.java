240867eb1e256125191cfc9349f7391f641991aa
Fr0sTByte12 <Nguyenkimdinh1412@gmail.com>
2024-08-29 18:39:41 +0700

    second one

---
+++ day1/test1.java
@@ -0,0 +1,7 @@
+package PR1.day1;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello moi nguoi");
+    }
+}
---
+++ day2/test2.java
@@ -0,0 +1,12 @@
+package PR1.day2;
+import java.util.Scanner;
+
+public class test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(i + b);
+    }
+
+}
