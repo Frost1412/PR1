10e804256f67cca248c4689747ade634dd8ba145
Fr0sTByte12 <Nguyenkimdinh1412@gmail.com>
2024-08-30 01:36:36 +0700

    update tut01 for ITCLC

---
+++ day1/tut01/Act5.java
@@ -0,0 +1,8 @@
+package PR1.day1.tut01;
+
+public class Act5 {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+
+    }
+}
---
+++ day1/tut01/Act6.java
@@ -0,0 +1,9 @@
+package PR1.day1.tut01;
+
+public class Act6 {
+    public static void main(String[] args) {
+        double math = ((9.5*4.5)-(2.5*3))/(45.5-3.5);
+        System.out.print(math);
+    }
+
+}
