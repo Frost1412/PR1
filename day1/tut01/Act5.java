5c45b74b295e88d498cc16cb66d6964977ca96a0
Fr0sTByte12 <Nguyenkimdinh1412@gmail.com>
2024-09-05 23:43:29 +0700

    update buoi 1

--- day1/tut01/Act5.java
+++ day1/tut01/Act5.java
@@ -2,7 +2,7 @@ package PR1.day1.tut01;
 
 public class Act5 {
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
-
+            System.out.println("Hello, World");
+//            Sau khi in ra hello world se di kem 1 dau enter xuong dong
     }
 }
--- day1/tut01/Act6.java
+++ day1/tut01/Act6.java
@@ -2,8 +2,14 @@ package PR1.day1.tut01;
 
 public class Act6 {
     public static void main(String[] args) {
-        double math = ((9.5*4.5)-(2.5*3))/(45.5-3.5);
-        System.out.print(math);
-    }
+//       Cho 34932s, tính số giây, số giờ và số phút
+    int h = 34932/60/60 ;
+    int m = 34932/60%60 ;
+    int s = 343932%60 ;
+        System.out.println(h+ "h " + m + "m " + s+ "s " );
 
+    }
 }
+//5 giờ 10 phút 2 giây
+//Gợi ý: 1 giờ 60 phút, 1 phút 60s
+// 3,15 phút => 3. Còn phần .15 sẽ chia cho 60 để lấy phần dư là số phút còn lại
